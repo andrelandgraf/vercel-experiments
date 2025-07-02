71a1467d3987aac66773c3638b292e7f0570f76b
Andre Landgraf <andre.timo.landgraf@gmail.com>
2025-07-01 22:51:55 -0700

    fix server template path

--- packages/app/src/entry.server.tsx
+++ packages/app/src/entry.server.tsx
@@ -1,12 +1,12 @@
 import { renderToString } from "react-dom/server";
 import { readFile } from "node:fs/promises";
-import { resolve } from "node:path";
+import { fileURLToPath } from "node:url";
 import { Router } from "framework/runtime";
 import { routes } from "./App";
 
 export async function GET(request: Request) {
   const url = new URL(request.url);
-  const templatePath = resolve(__dirname, "index.html");
+  const templatePath = fileURLToPath(new URL("./index.html", import.meta.url));
   let html = await readFile(templatePath, "utf8");
   const appHtml = renderToString(<Router routes={routes} url={url} />);
   html = html.replace('<div id="app"></div>', `<div id="app">${appHtml}</div>`);
--- packages/marketing/src/entry.server.tsx
+++ packages/marketing/src/entry.server.tsx
@@ -1,10 +1,12 @@
 import { renderToString } from "react-dom/server";
 import { readFile } from "node:fs/promises";
-import { resolve } from "node:path";
+import { fileURLToPath } from "node:url";
 import App from "./App";
 
 export async function GET() {
-  const templatePath = resolve(__dirname, "../static/index.html");
+  const templatePath = fileURLToPath(
+    new URL("../static/index.html", import.meta.url),
+  );
   let html = await readFile(templatePath, "utf8");
   const appHtml = renderToString(<App />);
   html = html.replace('<div id="app"></div>', `<div id="app">${appHtml}</div>`);
