13cce5c91f96cec62e73a8bee055c61b421652c0
Andre Landgraf <andre.timo.landgraf@gmail.com>
2025-07-01 22:58:34 -0700

    Add debug logs to build and server

--- packages/app/src/entry.server.tsx
+++ packages/app/src/entry.server.tsx
@@ -1,12 +1,19 @@
 import { renderToString } from "react-dom/server";
-import { readFile } from "node:fs/promises";
-import { resolve } from "node:path";
+import { readFile, readdir } from "node:fs/promises";
+import { fileURLToPath } from "node:url";
 import { Router } from "framework/runtime";
 import { routes } from "./App";
 
 export async function GET(request: Request) {
   const url = new URL(request.url);
-  const templatePath = resolve(__dirname, "index.html");
+  const templatePath = fileURLToPath(new URL("./index.html", import.meta.url));
+
+  const dir = fileURLToPath(new URL("./", import.meta.url));
+  console.log("📂 Server cwd", process.cwd());
+  console.log("📂 Server dir", dir);
+  console.log("📂 Dir contents", await readdir(dir));
+  console.log("📄 Template path", templatePath);
+
   let html = await readFile(templatePath, "utf8");
   const appHtml = renderToString(<Router routes={routes} url={url} />);
   html = html.replace('<div id="app"></div>', `<div id="app">${appHtml}</div>`);
